971cd6f5016e7aad0253b2557c002bb9276f3ecb
Jorge <email@whoisjorge.me>
2022-03-19 08:18:08 +0100

    fix: cpt name was breaking wp admin

--- includes/core/Instance.php
+++ includes/core/Instance.php
@@ -1,6 +1,12 @@
 <?php
 namespace ThemeName\Core;
 
+/**
+ * Set theme class instances.
+ *
+ * @package ThemeName
+ */
+
 if (!defined('ABSPATH')) {
     exit(); // Exit if accessed directly.
 }
@@ -9,23 +15,23 @@ abstract class Instance {
     /**
      * Holds the module instance.
      */
-    protected static $_instances = [];
+    private static $_instances = [];
 
     /**
      * Ensures only one instance of the module class is loaded or can be loaded.
      */
     public static function instance() {
         $class_name = static::class_name();
-
         if (empty(static::$_instances[$class_name])) {
             static::$_instances[$class_name] = new static();
         }
-
+        // var_dump(static::$_instances[$class_name]);
         return static::$_instances[$class_name];
     }
 
     /**
      * Retrieve the name of the class.
+     * @return string
      */
     public static function class_name() {
         return get_called_class();
