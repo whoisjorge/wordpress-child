a2c777318277b9a27a2ab62a60e80d4a1be17e53
Jorge <email@whoisjorge.me>
2023-01-27 09:25:05 +0100

    refactor: make relative some specific module assets

--- includes/admin/AdminFrontend.php
+++ includes/admin/AdminFrontend.php
@@ -18,6 +18,13 @@ class AdminFrontend extends ModuleInstance {
 	}
 
 	/**
+	 * @return String /wp-content/themes/<theme_name>/includes/admin
+	 */
+	private function get_relative_path() {
+		return strstr( dirname( __FILE__ ), '/wp-content' );
+	}
+
+	/**
 	 * Hide menus from the admin in the client session.
 	 * @since 1.0.0
 	 * @link https://developer.wordpress.org/reference/functions/remove_menu_page/
@@ -27,6 +34,11 @@ class AdminFrontend extends ModuleInstance {
 		// remove_menu_page('edit.php?post_type=elementor_library');
 		// remove_menu_page('envato-elements');
 		// remove_menu_page('jet-engine');
+
+		// remove_submenu_page( 'themes.php', 'themes.php' );
+
+		// $customizer_url = add_query_arg( 'return', urlencode( remove_query_arg( wp_removable_query_args(), wp_unslash( $_SERVER['REQUEST_URI'] ) ) ), 'customize.php' );
+		// remove_submenu_page( 'themes.php', $customizer_url );
 	}
 
 	/**
@@ -56,7 +68,7 @@ class AdminFrontend extends ModuleInstance {
 	 * @link https://developer.wordpress.org/reference/hooks/admin_footer/
 	 */
 	public function footer_text_version() {
-		$wp_logo    = file_get_contents( THEME_NAME_PATH . '/assets/admin/wordpress.svg' );
+		$wp_logo    = file_get_contents( dirname( __FILE__ ) . '/assets/img/wordpress-icon.svg' );
 		$wp_version = get_bloginfo( 'version', 'display' );
 
 		return '<small>Theme_Name ' . THEME_NAME_VERSION . ' — <span style="vertical-align:sub">' . $wp_logo . '</span> ' . $wp_version . '</small>';
@@ -68,7 +80,7 @@ class AdminFrontend extends ModuleInstance {
 	 * @link https://developer.wordpress.org/reference/hooks/admin_enqueue_scripts/
 	 */
 	public function enqueue_admin_frontend_style() {
-		wp_register_style( 'admin_frontend_css', THEME_NAME_ASSETS_URI . '/admin/admin.css', false, THEME_NAME_VERSION );
+		wp_register_style( 'admin_frontend_css', $this->get_relative_path() . '/assets/css/style.css', false, THEME_NAME_VERSION );
 		wp_enqueue_style( 'admin_frontend_css' );
 	}
 
@@ -80,6 +92,7 @@ class AdminFrontend extends ModuleInstance {
 		if ( ! has_filter( 'admin_footer_text' ) && ! has_filter( 'update_footer' ) ) {
 			add_filter( 'admin_footer_text', array( $this, 'footer_text_developed_by' ), 11 );
 			add_filter( 'update_footer', array( $this, 'footer_text_version' ), 11 );
+
 			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_frontend_style' ) );
 		}
 
@@ -87,6 +100,21 @@ class AdminFrontend extends ModuleInstance {
 		if ( current_user_can( 'editor' ) ) {
 			add_action( 'admin_init', array( $this, 'delete_editor_menu_items' ) );
 		}
+
+		/*
+		 * ********************************************************************
+		 * 	Admin Tweaks (to do: create a function)
+		 * ********************************************************************
+		 */
+
+		// Removes WordPress contextual help tab
+		add_action(
+			'admin_head',
+			function() {
+				$screen = get_current_screen();
+				$screen->remove_help_tabs();
+			}
+		);
 	}
 }
 
